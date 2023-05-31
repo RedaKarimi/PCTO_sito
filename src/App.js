f1f8176355044ec82695c5163295827542813a29
RedaKarimi <redakarimi76@gmail.com>
2023-05-31 18:17:47 +0200

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	modified:   src/App.js 	new file:   src/img/asdasdad.webp 	new file:   src/img/campaign.jpg 	new file:   src/img/celebration.webp 	new file:   src/img/celebration2.jpg 	new file:   src/img/completoTennisDonna.jpg 	new file:   src/img/game.jpg 	new file:   src/img/logoMigliore.png 	new file:   src/img/model.jpg 	new file:   src/img/model3.jpg 	new file:   src/img/model4.png 	new file:   src/img/modelRosa.jpg 	deleted:    src/img/polo.jpg 	new file:   src/img/poloDonna.jpg 	new file:   src/img/poloImage.jpg 	new file:   src/img/poloOriginale.jpg 	new file:   src/img/poloTennis.jpg 	new file:   src/img/rglogo.png 	deleted:    src/img/throwback.jpg 	new file:   src/img/trophy.webp 	new file:   website/index.js

--- src/App.js
+++ src/App.js
@@ -1,115 +1,141 @@
+import "./App.css";
+import 'antd/dist/reset.css';
+import { Form,Button, Tooltip,Input,Layout, Space, Menu, Image, } from "antd";
+import { CSSTransition } from 'react-transition-group';
+import {
+  AppstoreOutlined,
+  MailOutlined,
+  SearchOutlined,
+  InstagramOutlined,
+  TwitterOutlined,
+  FacebookOutlined,
+  YoutubeOutlined,
+  ShoppingOutlined,
+} from "@ant-design/icons";
+import React, { useState,useRef } from "react";
+import Axios from "axios";
+const { Header, Content, Footer } = Layout;
+const onSearch = (value) => console.log(value);
 
-import './App.css';
-import {Layout, Menu,Image} from 'antd';
-import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
-import { useState } from 'react';
-
-const { Header, Content,} = Layout;
-
-let logo = require("./img/Lacoste-Logo.png")
-let immagine1 = require("./img/djokovic.jpg")
-let immagine2 = require("./img/polo.jpg")
-
-
-const items = [
-  {
-    label: 'Navigation One',
-    key: 'mail',
-    icon: <MailOutlined />,
+/* eslint-disable no-template-curly-in-string */
+const validateMessages = {
+  types: {
+    email: 'This email is not valid'
   },
-  {
-    label: 'Navigation Two',
-    key: 'app',
-    icon: <AppstoreOutlined />,
-    disabled: true,
-  },
-  {
-    label: 'Navigation Three - Submenu',
-    key: 'SubMenu',
-    icon: <SettingOutlined />,
-    children: [
-      {
-        type: 'group',
-        label: 'Item 1',
-        children: [
-          {
-            label: 'Option 1',
-            key: 'setting:1',
-          },
-          {
-            label: 'Option 2',
-            key: 'setting:2',
-          },
-        ],
-      },
-      {
-        type: 'group',
-        label: 'Item 2',
-        children: [
-          {
-            label: 'Option 3',
-            key: 'setting:3',
-          },
-          {
-            label: 'Option 4',
-            key: 'setting:4',
-          },
-        ],
-      },
-    ],
-  },
-  {
-    label: (
-      <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-        Navigation Four - Link
-      </a>
-    ),
-    key: 'alipay',
-  },
-];
+};
+/* eslint-enable no-template-curly-in-string */
+const onFinish = (values) => {
+  console.log(values);
+};
 
 function App() {
+  const [email, setEmail] = useState("");  
+  const [isOnSearch, setIsOnSearch] = useState(false);
+  const nodeRef = useRef(null);
+
+  const sendEmail = () => {
+    Axios.post("http://192.168.250.41:8080/email", {
+      getEmail: email,
+    }).then((response) => {
+      console.log(response);
+    });
+  };
   return (
-    <Layout className="layout">
-      <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
-
-      <div class="logo">
-      <img src={logo} alt="test" width="8.5%"></img>
-        </div>
-        <Menu
-          theme="light"
-          mode="horizontal"
-          defaultSelectedKeys={['1']}
-          items={items}
-        />
-      <Content style={{ padding: '0 50px' }}>
-      </Content>
-        
-        <i class="fas fa-comment-alt"></i>
-
-  <div class="container">
-      <div class="container1">
-        <img class="immagine" src={immagine1}  alt="test2"></img>
-        <div class="overlay1">
-          <div class="text">
-            TENNIS & ROLAND GARROS
-          </div>
-        </div>       
-      </div>
-
-      <div class="container2">
-        <img class="immagine" src={immagine2}  alt="test2"></img>
-          <div class="overlay2">
-            <div class="text">
-              POLO AND LUXURY
-            </div>
-          </div>  
-      </div>
-    </div>
-
-    <footer>
+    <Layout className="elements">
+      <Header style={{display: "flex",height: 105,margin: -10,color: "white",backgroundColor: "white",alignItems: "center", position:"fixed", top:0, left:10, right:0, zIndex: 1000, width:"100%"}}>
+        <img
+          src={require("./img/logoMigliore.png")}
+          alt="test"
+          width="6%"
+        ></img>
+          <Menu 
+          style={{width: "100%",margin: 0,padding: 20,height: 0,display: "flex",justifyContent: "flex-end",alignItems: "center",}}
+          disabledOverflow={true}
+          theme={{
+            token: {
+              colorPrimary: '#005226',
+            },
+          }}
       
-    </footer>
+          mode="horizontal"
+          breakpoint="lg"
+          collapsedWidth="0"
+          defaultSelectedKeys={["0"]}>
+  
+          <Menu.Item key="mail" className="modifica" style={isOnSearch===true?{marginTop:-20}:{marginTop: -15}} >
+            <Tooltip title="search">
+              <Button size="large" className="items" type="text" shape="circle" icon={<MailOutlined style={{fontSize:"23px", color:"black"}} />}  />
+            </Tooltip>
+          </Menu.Item>
+          <Menu.Item key="app" style={isOnSearch===true?{marginTop:-20}:{marginTop: -15}}>
+          <Button  size="large" className="items" type="text" shape="circle" icon={<AppstoreOutlined style={{fontSize:"23px", color:"black "}}  />} />
+          </Menu.Item>
+          <Menu.Item key="shop" style={isOnSearch===true?{marginTop:-20}:{marginTop: -15}}>
+          <Button  size="large" className="items" type="text" shape="circle" icon={ <ShoppingOutlined style={{fontSize:"23px", color:"black "}} />} />
+          </Menu.Item>
+          <Menu.Item  key="search">
+            { isOnSearch &&
+              (<Input placeholder="input search text" allowClear onSearch={onSearch} style={{borderColor:"#015335"}}/>
+              )}
+              <Button size="large" type="text" shape="circle" icon={ <SearchOutlined  style={{fontSize:"23px" }} />} onClick={()=>{setIsOnSearch(!isOnSearch)}} />
+          </Menu.Item>
+        </Menu> 
+      </Header>
+      <Content className="content">
+        <div class="container">
+          <div class="container1">
+            <img
+              class="immagine"
+              src={require("./img/trophy.webp")}
+              alt="test2"
+            ></img>
+          </div>
+        </div>
+
+        <div class="contenitore">
+          <a href="#"><img src={require("./img/rglogo.png")} class="image1"/><p class="carousel">ROLAND GARROS</p></a>
+          <a href="#"><img src={require("./img/model3.jpg")} class="image2"/><p class="carousel"> POLO & LUXURY</p></a>
+        </div>
+
+        <div class="caroselloImmagini">
+            <img src={require("./img/poloOriginale.jpg")}></img>
+            <img src={require("./img/poloTennis.jpg")}></img>
+            <img src={require("./img/poloDonna.jpg")}></img>
+            <img src={require("./img/completoTennisDonna.jpg")}></img>
+        </div>
+      </Content>
+      <Footer className="footer">
+        <div class="social">
+          <InstagramOutlined className="icone" />
+          <TwitterOutlined className="icone" />
+          <FacebookOutlined className="icone" />
+          <YoutubeOutlined className="icone" />
+        </div>
+
+        <table>
+          <thead>
+            <tr><th>Riguardo Lacoste</th><th>Categorie</th><th>Aiuto e Contatti</th><th>Unisciti al club Lacoste</th></tr>
+          </thead>
+          <tbody>
+            <tr><td>ciao</td><td>ciao di nuovo</td><td>ciao 3</td><td>
+            <Form
+            className="inserimentoMail"
+              name="nest-messages"
+              onFinish={onFinish}
+              validateMessages={validateMessages}
+            >
+            <Form.Item name={['user', 'email']} rules={[{ type: 'email' }]}>
+            <Input className="cellaMail"
+            placeholder="La tua e-mail" allowClear onChange={(e) => {setEmail(e.target.value)}}
+            onPressEnter={sendEmail} style={{ width: 200, borderRadius: "50px", border: "none"}} 
+            />
+            </Form.Item>
+            <button class="inserimentoDati" onClick={sendEmail} >Iscriviti</button>
+            </Form>
+              </td></tr>
+          </tbody>
+        </table>
+      </Footer>
     </Layout>
   );
 }
